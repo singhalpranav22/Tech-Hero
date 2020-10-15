678c002e90d91ed8402336a973777ad65a0a1cec
smitsgithub <70274408+smitsgithub@users.noreply.github.com>
2020-10-15 16:44:59 +0530

    Update bootstrap.bundle.js

--- assets/vendor/bootstrap/js/bootstrap.bundle.js
+++ assets/vendor/bootstrap/js/bootstrap.bundle.js
@@ -1,8 +1,4 @@
-/*!
-  * Bootstrap v4.5.0 (https://getbootstrap.com/)
-  * Copyright 2011-2020 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
-  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
-  */
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
   typeof define === 'function' && define.amd ? define(['exports', 'jquery'], factory) :
